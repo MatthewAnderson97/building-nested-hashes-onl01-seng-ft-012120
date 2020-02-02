50f9043fe2f834f9cae0414dd127f4ae7700f82c
Matthew Anderson <anderson.matt1397@gmail.com>
2020-02-02 10:02:19 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,7 +24,7 @@ def fourth_challenge
         :second_friend => { 
           name: "Nurse",
           age: "44",
-          attitude: "worried"#your hashes here!
+          attitude: "worried"}#your hashes here!
       ]
    }
 }
