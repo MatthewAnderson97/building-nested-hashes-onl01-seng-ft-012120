4a251ba632e73930eff3c77c6816ec7e6ebc48b1
Matthew Anderson <anderson.matt1397@gmail.com>
2020-02-02 09:43:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,29 +2,33 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-         name: "Lord Montague"
+         name: "Lord Montague",
          age: "53"#your key/value pairs here
         },
       :matriarch => {
-        name: "Lady Montague"
+        name: "Lady Montague",
         age: "54"#your key/value pairs here
         },
       :hero => {
-        name: "Romeo"
-        age: "15"#your key/value pairs here
+        name: "Romeo",
+        age: "15",
+        status: "alive"#your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet"
-        age: "50#your key/value pairs here
+        name: "Lord Capulet",
+        age: "50"#your key/value pairs here
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet",
+        age: "50"#your key/value pairs here
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet",
+        age: "15",
+        status: "alive"#your key/value pairs here
         },
       :heroine_friends => []
    }
